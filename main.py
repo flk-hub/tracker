994c919cb6c00e2729bb814e3ddb11422220dad9
ivaniolukau@gmail.com <ivaniolukau@gmail.com>
2023-07-02 15:31:41 +0100

    added daisy ui

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-"""Process entry points"""
+"""Process entry points."""
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+
+data = [{"name": "ikl"}]
+
+
+@app.get("/get_users")
+def get_users():
+    """Get users."""
+    return data
+
 
 # Place After All Other Routes
 app.mount("", StaticFiles(directory="client/dist/", html=True), name="static")
--- makefile
+++ makefile
@@ -4,8 +4,13 @@ setup:
 	source venv/bin/activate
 	venv/bin/pip install -r requirements.txt
 	npm install --prefix client
-run: setup
-	venv/bin/uvicorn main:app --reload 
 
 build-client:
 	npm run build --prefix client
+
+run: build-client
+	venv/bin/uvicorn main:app --reload 
+
+
+run-dev-client:
+	npm run dev --prefix client
